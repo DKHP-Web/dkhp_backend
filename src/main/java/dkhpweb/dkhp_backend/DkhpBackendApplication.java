0ee8bcd4913d3c8b02c1ebee5517ff722e10af53
HungTran170904 <tienhung17092004@gmail.com>
2025-02-23 21:27:52 +0700

    feat: init project

---
+++ src/main/java/dkhpweb/dkhp_backend/DkhpBackendApplication.java
@@ -0,0 +1,13 @@
+package dkhpweb.dkhp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkhpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DkhpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dkhpweb/dkhp_backend/DkhpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dkhpweb.dkhp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkhpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
